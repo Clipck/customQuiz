aebdc9a583f74c08747ef630ede05636dd6e7550
Clipck <97559351+Clipck@users.noreply.github.com>
2024-03-13 03:02:33 -0700

    Got started on adding question display and buttons
    Didn't wanna finish off here but I gotta sleep so I'm leaving this as a checkpoint
    
    Buttons currently call a function that doesn't do anything, but they do convey their associated "value"

--- pmdQuiz/cores/uiCore/uiComponents/coreGame/uiComp_coreGame.gd
+++ pmdQuiz/cores/uiCore/uiComponents/coreGame/uiComp_coreGame.gd
@@ -1,10 +1,20 @@
 extends Control
 
+#Shorthand
+@onready var displayBox = $Panel/displayBox;
+
+func _process(delta):
+	displayBox.text = GameCore.dbCore.questionList[0].displayText;
+
 """
 =======
 SIGNALS
 =======
 """
+
+func _on_b_answer_pressed(buttonNumber):
+	print(buttonNumber)
+	answerChoice(buttonNumber);
 
 func _on_b_goto_exit_quiz_pressed():
 	exitQuiz();
@@ -15,5 +25,9 @@ BUTTONS
 =======
 """
 
+func answerChoice(answerNumber):
+	pass;
+
 func exitQuiz():
 	GameCore.gotoMainMenu();
+
